3e4f686dda7081dcae5e3c433a63e2805dc87db7
Yansimin-code <78136451+Yansimin-code@users.noreply.github.com>
2021-01-28 20:38:20 +0100

    Create two step forecasting.py

---
+++ two step forecasting.py
@@ -0,0 +1,2 @@
+#The goal of this notebook is to predict MWh grid loss in power grids using machine learning.
+#The dataset is from https://www.kaggle.com/trnderenergikraft/grid-loss-time-series-dataset
